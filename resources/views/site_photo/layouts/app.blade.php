97bbbc60c175ce60a53752d532e3383bf4947e04
Sergey <1@php-cat.com>
2024-01-01 14:29:49 +0500

    + метрика в кружки

--- resources/views/site_photo/layouts/app.blade.php
+++ resources/views/site_photo/layouts/app.blade.php
@@ -84,6 +84,23 @@
 
 </body>
 
+{{--<!-- Yandex.Metrika counter -->--}}
+<script type="text/javascript" >
+    (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(96033624, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true
+    });
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/96033624" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+{{--<!-- /Yandex.Metrika counter -->--}}
+
 <script src="{{ asset('site_photo/js/app.js') }}?{{ rand(1, 999999) }}"></script>
 <script src="./TW-ELEMENTS-PATH/dist/js/index.min.js"></script>
 
